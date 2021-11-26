2ef8804d02565d791f927d71fe96ebf1f8138f8a
dlwndrhs05 <dlwndrhs05@gmail.com>
2021-11-26 11:43:58 +0900

    chat setting up

--- src/com/chatting/Chat/ChatDAO.java
+++ src/com/chatting/Chat/ChatDAO.java
@@ -1,4 +1,31 @@
 package com.chatting.Chat;
 
+
+import java.sql.*;
+
 public class ChatDAO {
+    private Connection con;
+    private ResultSet rs;
+    private PreparedStatement pstmt;
+
+    public ChatDAO() {
+        try {
+            String URL = "jdbc:mysql://localhost:3306/jsp_web_commu?useUnicode=true&characterEncoding=UTF-8&characterSetResults=UTF-8";
+            String id = "root";
+            String password = "2468";
+
+            con = DriverManager.getConnection(URL, id, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Chat(String nickname, String chat, String createDate) {
+        String sql = "select nickname,chat,createDate from chat";
+        try {
+            pstmt = con.prepareStatement(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
